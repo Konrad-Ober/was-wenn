05619a3393ef30a6ee2ce95ed5a21db0f4fe90b9
Konrad-Ober <was-wenn-dann@gmx.de>
2020-10-06 13:54:27 +0200

    Updates

---
+++ precache-manifest.a0c557689034652d031f1464d2064790.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa251568323fda496b69538d6c01771",
+    "url": "/Konrad-Ober/was-wenn.git/index.html"
+  },
+  {
+    "revision": "1b1bbbcd76e79ee2fdf8",
+    "url": "/Konrad-Ober/was-wenn.git/static/css/main.24eeadfc.chunk.css"
+  },
+  {
+    "revision": "be65c25019d6c7bd5e9b",
+    "url": "/Konrad-Ober/was-wenn.git/static/js/2.7615f980.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Konrad-Ober/was-wenn.git/static/js/2.7615f980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b1bbbcd76e79ee2fdf8",
+    "url": "/Konrad-Ober/was-wenn.git/static/js/main.01b93e02.chunk.js"
+  },
+  {
+    "revision": "e2f5bd604fc604170d11",
+    "url": "/Konrad-Ober/was-wenn.git/static/js/runtime-main.a316df42.js"
+  },
+  {
+    "revision": "4e5d3b36eb2e109a6b9750a2190c3355",
+    "url": "/Konrad-Ober/was-wenn.git/static/media/Info.4e5d3b36.JPG"
+  },
+  {
+    "revision": "c1c9165298a48bfd67f84d16971430e9",
+    "url": "/Konrad-Ober/was-wenn.git/static/media/fake.c1c91652.jpg"
+  },
+  {
+    "revision": "a4392bfaef736a80aa5c60f6a4e0f308",
+    "url": "/Konrad-Ober/was-wenn.git/static/media/grundrechte.a4392bfa.JPG"
+  },
+  {
+    "revision": "104157efc514d628d3aab19276a427da",
+    "url": "/Konrad-Ober/was-wenn.git/static/media/grundrechte2.104157ef.JPG"
+  },
+  {
+    "revision": "b3079cbe376181cc82f689501111dfc7",
+    "url": "/Konrad-Ober/was-wenn.git/static/media/immun1.b3079cbe.JPG"
+  },
+  {
+    "revision": "c40ca868f5c062ba5bdd58fdfa8a1e76",
+    "url": "/Konrad-Ober/was-wenn.git/static/media/impfen1.c40ca868.JPG"
+  },
+  {
+    "revision": "67cee3f761776f2fa1aab63f8ae84006",
+    "url": "/Konrad-Ober/was-wenn.git/static/media/kinder1.67cee3f7.JPG"
+  },
+  {
+    "revision": "2ea8ac791f300a7ed757b55ce29c012d",
+    "url": "/Konrad-Ober/was-wenn.git/static/media/kinder2.2ea8ac79.JPG"
+  },
+  {
+    "revision": "0eb91d607a8dc3ef9137ff668eca2ac7",
+    "url": "/Konrad-Ober/was-wenn.git/static/media/lachen.0eb91d60.JPG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Konrad-Ober/was-wenn.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "77870171844d65dd4f8859c4e67d48f3",
+    "url": "/Konrad-Ober/was-wenn.git/static/media/zweifel1.77870171.JPG"
+  }
+]);
